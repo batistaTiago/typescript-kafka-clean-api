e64ecdfd72282216b086356c803624ccdd420221
tiago <tiago.batista@olistpax.com>
2022-10-14 16:10:22 -0300

    parameterize topic name in worker script

--- src/application/worker.ts
+++ src/application/worker.ts
@@ -1,8 +1,7 @@
 import 'reflect-metadata';
 import { WorkerApplication } from './worker-application';
 
-// @@TODO: topic name deve vir por parametro do script.
-const TOPIC_NAME = 'events';
+const TOPIC_NAME = process.argv.slice(2)[0] ?? 'events';
 
 new WorkerApplication(TOPIC_NAME).start().then(() => {
     console.log('Worker running...');
