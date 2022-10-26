254ecec7922664426f131ad8042248bcfc4dd6a2
tiago <tiago.batista@olistpax.com>
2022-10-26 16:52:04 -0300

    improve express routing implementation

--- src/infra/http/express/controllers/generate-verification-code-controller-express-adapter.ts
+++ src/infra/http/express/controllers/generate-verification-code-controller-express-adapter.ts
@@ -5,7 +5,6 @@ import { Controller } from '../../../../domain/services/http/controller';
 import { ErrorResilientController } from '../../../../domain/services/http/error-resilient-controller';
 import { HttpResponse } from '../../../../domain/services/http/http-response';
 import { ExpressControllerAdapter } from '../express-controller-adapter';
-import { ExpressRoute } from '../express-route';
 
 @injectable()
 export class GenerateVerificationCodeControllerExpressAdapter extends ExpressControllerAdapter {
@@ -15,10 +14,6 @@ export class GenerateVerificationCodeControllerExpressAdapter extends ExpressCon
         super();
 
         this.domainController = new ErrorResilientController(domainController);
-    }
-
-    public route(): ExpressRoute {
-        return { method: 'get', url: '/verification-code' };
     }
 
     protected async handleExpressRequest(req: Request): Promise<HttpResponse> {
---
+++ src/infra/http/express/routes.ts
@@ -0,0 +1,13 @@
+import GenericConstructor from "../../../utils/generic-constructor-type";
+import { FindUserControllerExpressAdapter } from "./controllers/find-user-controller-express-adapter";
+import { GenerateVerificationCodeControllerExpressAdapter } from "./controllers/generate-verification-code-controller-express-adapter";
+import { HomeControllerExpressAdapter } from "./controllers/home-controller-express-adapter";
+import { SignUpControllerExpressAdapter } from "./controllers/sign-up-controller-express-adapter";
+import { ExpressRoute } from "./express-route";
+
+export const Routes: Array<ExpressRoute> = [
+    { url: '/', method: 'get', controller: HomeControllerExpressAdapter },
+    { url: '/verification-code', method: 'get', controller: GenerateVerificationCodeControllerExpressAdapter },
+    { url: '/auth/sign-up', method: 'post', controller: SignUpControllerExpressAdapter },
+    { url: '/users/:id', method: 'get', controller: FindUserControllerExpressAdapter },
+];
