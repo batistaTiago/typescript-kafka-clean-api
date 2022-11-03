22fede38fb5c8c5844166152b802393752b19225
tiago <tiago.batista@olistpax.com>
2022-11-03 13:13:54 -0300

    refactor user repository with interface segregation

--- src/domain/services/repositories/user-repository.ts
+++ src/domain/services/repositories/user-repository.ts
@@ -1,8 +1,18 @@
 import { UserModel } from "../../../infra/models/user-model";
 import { SignUpDTO, SignUpDTOModel } from "../../dto/sign-up";
 
-export interface UserRepository {
+export interface StoreUserRepository {
     storeUser(user: SignUpDTO): Promise<SignUpDTOModel>;
+}
+
+export interface FindByIdUserRepository {
     findById(id: string): Promise<UserModel>;
+}
+
+export interface FindByEmailUserRepository {
     findByEmail(email: string): Promise<UserModel>;
 }
+
+
+export declare type UserRepository = StoreUserRepository & FindByIdUserRepository & FindByEmailUserRepository;
+// export interface UserRepository extends StoreUserRepository, FindByIdUserRepository, FindByEmailUserRepository {}
