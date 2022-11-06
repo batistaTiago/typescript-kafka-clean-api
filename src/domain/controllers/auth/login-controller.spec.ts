02b4e7d9fb15791a3b518f124a247aa8c9655e04
tiago <tiago.batista@olistpax.com>
2022-11-06 14:31:36 -0300

    add first test to login controller

--- src/domain/controllers/auth/login-controller.spec.ts
+++ src/domain/controllers/auth/login-controller.spec.ts
@@ -1,5 +1,14 @@
+import { UseCase } from "../../use-cases/use-case";
+import { LoginController } from "./login-controller";
+
 describe('LoginController', () => {
-    it.skip('tbd', () => {
-        expect(true).toBeTruthy();
-    })
-})
+    it('should forward call into the usecase', async () => {
+        const useCase = { execute: jest.fn() } as unknown as UseCase;
+        const executeSpy = jest.spyOn(useCase, 'execute');
+
+        const sut = new LoginController(useCase);
+        await sut.handle({ body: { some: 'data' } });
+
+        expect(executeSpy).toHaveBeenCalledWith({ some: 'data' });
+    });
+});
