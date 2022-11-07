df767063831667ca95f48aaf3d6e74f7d6af7e9f
tiago <tiago.batista@olistpax.com>
2022-11-07 18:12:37 -0300

    improve generate code integration test

--- tests/feature/generate-code/generate-code.test.ts
+++ tests/feature/generate-code/generate-code.test.ts
@@ -6,27 +6,35 @@ import { MongoClient } from "mongodb";
 import { Environment } from "../../../src/config/environment";
 import { AuthenticateUser } from '../../../src/infra/http/express/middleware/authenticate-user';
 
-const jwt: Encrypter = container.resolve('Encrypter');
-const api = global.expressTestServer;
-const client = new MongoClient(Environment.MONGO_CONNECTION_URI as any);
-const factory = new UserFactory();
-Environment.APP_DEBUG = false;
-
-const makeRequest = async (token: string) => await request(api).get('/verification-code').set('Authorization', `Bearer ${token}`);
-
 describe('Code Generation API', () => {
+    const jwt: Encrypter = container.resolve('Encrypter');
+    const api = global.expressTestServer;
+    const client = new MongoClient(Environment.MONGO_CONNECTION_URI as any);
+    const factory = new UserFactory();
+    Environment.APP_DEBUG = false;
+    
+    const makeRequest = async (token: string) => await request(api).get('/verification-code').set('Authorization', `Bearer ${token}`);
+
+    beforeAll(async () => {
+        await client.connect();
+    });
+
+    afterAll(async () => {
+        await client.connect();
+    });
+
     beforeEach(async () => {
         await client.db().dropDatabase();
     });
 
-    it.skip('should call the authentication middleware', async () => {
-        const middleware = container.resolve(AuthenticateUser);
-        const applySpy = jest.spyOn(middleware, 'apply');
-        await request(api)
-            .get('/verification-code');
+    // it.skip('should call the authentication middleware', async () => {
+    //     const middleware = container.resolve(AuthenticateUser);
+    //     const applySpy = jest.spyOn(middleware, 'apply');
+    //     await request(api)
+    //         .get('/verification-code');
 
-        expect(applySpy).toHaveBeenCalled();
-    });
+    //     expect(applySpy).toHaveBeenCalled();
+    // });
 
     it('should return unauthenticated if no token is provided', async () => {
         const response = await request(api).get('/verification-code');
@@ -62,5 +70,42 @@ describe('Code Generation API', () => {
         expect(response.body.expiresAt).toBeDefined();
         expect(secondResponse.body.code).toEqual(response.body.code);
         expect(secondResponse.body.expiresAt).toEqual(response.body.expiresAt);
+    });
+
+    it('should create a single record in the database for the current user even if called multiple times', async () => {
+        const user = await factory.create({
+            name: "testuser",
+            email: "test@email.dev",
+            registrationDate: new Date(),
+            password: "userpassword",
+        });
+
+        const token = jwt.encrypt({ id: user.id, issuedAt: new Date() });
+
+        expect((await client.db().collection('verification_codes').find({}).toArray()).length).toEqual(0);
+
+        await makeRequest(token);
+        await makeRequest(token);
+
+        const verificationCodes = await client.db().collection('verification_codes').find({ "user.email": 'test@email.dev' }).toArray();
+
+        expect(verificationCodes.length).toEqual(1);
+    });
+
+    it('should create a record in the database for the current user', async () => {
+        const user = await factory.create({
+            name: "testuser",
+            email: "test@email.dev",
+            registrationDate: new Date(),
+            password: "userpassword",
+        });
+
+        const token = jwt.encrypt({ id: user.id, issuedAt: new Date() });
+
+        const response = await makeRequest(token);
+        
+        const verificationCode = await client.db().collection('verification_codes').findOne({ code: response.body.code, "user.email": 'test@email.dev' });
+
+        expect(verificationCode).toBeTruthy();
     });
 });
