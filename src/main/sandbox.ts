e206102715a8e975a97a0bd2100318ed3668b8a5
batista <ekyidag@gmail.com>
2023-02-27 19:19:49 -0300

    update .env.example

--- src/main/sandbox.ts
+++ src/main/sandbox.ts
@@ -1,20 +1,40 @@
-import 'reflect-metadata';
-import { container } from 'tsyringe';
-import { Event } from '../domain/entities/event';
-import { Events } from '../domain/enums/events';
-import { Encrypter } from '../domain/services/cryptography/encrypter';
-import * as Providers from '../infra/providers';
+const express = require('express');
 
-Providers.registerAll();
+const app = express();
 
-const encrypter: Encrypter = container.resolve('Encrypter');
-const event: Event = {
-    eventName: Events.NEW_SANDBOX_EXECUTION,
-    happenedAt: new Date(),
-    data: {
-        some: 'data'
+const isPrime = (num) => {
+    if (num <= 1) return false;
+    for (let i = 2; i < num - 1; i++) {
+        if (num % i === 0) {
+            return false;
+        }
     }
+
+    return true;
 }
 
-const jwtString = encrypter.encrypt(event);
-const decrypted = encrypter.decrypt(jwtString);
+app.get('/:count', (req, res) => {
+
+    let primes = [];
+    let n = 0;
+    const input = Number(req.params.count);
+    const startTimestamp = new Date().getTime();
+    while (primes.length < input) {
+        if (isPrime(n)) {
+            primes.push(n);
+        }
+
+        n++;
+    }
+
+    const endTimestamp = new Date().getTime();
+
+    const elapsedTime = endTimestamp - startTimestamp;
+
+    res.json({ input, elapsedTime, primes });
+});
+
+
+app.listen(3000, () => {
+    console.log('server runnning...');
+});
